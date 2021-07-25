55e364969bd5c0098aa08c7d311b8edd749465cf
Joe Valencia <stiwart@gmail.com>
2021-07-24 23:59:50 -0500

    Add field to user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,11 +16,17 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('last_name');
+            $table->unsignedInteger('identification_type_id')->nullable();
+            $table->string('identification_number');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+
+            $table->foreign('identification_type_id')->references('id')
+            ->on('identification_types')->onDelete('cascade');
         });
     }
 
